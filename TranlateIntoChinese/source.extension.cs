6c45714277cf4b46e7d2e1b6306d2ae30f1b2b78
Entity-now <2966388213@qq.com>
2023-09-26 20:33:09 +0800

    修复版本guid不匹配的问题

--- TranlateIntoChinese/source.extension.cs
+++ TranlateIntoChinese/source.extension.cs
@@ -7,11 +7,11 @@ namespace TranlateIntoChinese
 {
     internal sealed partial class Vsix
     {
-        public const string Id = "TranlateIntoChinese.d5663168-0b3d-483d-9665-f8bdc90e94aa";
+        public const string Id = "vsix_EnTranslate.922bb0c3-ecc3-4e62-b60e-6a51f090bc7f";
         public const string Name = "TranlateIntoChinese";
-        public const string Description = @"Empty VSIX Project.";
+        public const string Description = @"EnTranslate是一款功能强大的离线划词翻译插件，拥有超过340万条离线词库，能够准确翻译各种生僻单词。作为Visual Studio的VSIX插件，它为您提供了出色的英语单词翻译功能，而且具备灵活的单词拆分能力，支持驼峰命名和下划线命名等多种形式的单词拆分。无论您是在编写代码还是阅读文档，EnTranslate都是您的最佳翻译助手。";
         public const string Language = "zh-Hans";
-        public const string Version = "1.0";
+        public const string Version = "1.3.0";
         public const string Author = "DDD D";
         public const string Tags = "";
     }
