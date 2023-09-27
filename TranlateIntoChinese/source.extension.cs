1afefa0ff574e0827260e70f4c7dfa856378423c
Entity-now <2966388213@qq.com>
2023-09-27 22:07:40 +0800

    fix config

--- TranlateIntoChinese/source.extension.cs
+++ TranlateIntoChinese/source.extension.cs
@@ -11,8 +11,8 @@ namespace TranlateIntoChinese
         public const string Name = "TranlateIntoChinese";
         public const string Description = @"EnTranslate是一款功能强大的离线划词翻译插件，拥有超过340万条离线词库，能够准确翻译各种生僻单词。作为Visual Studio的VSIX插件，它为您提供了出色的英语单词翻译功能，而且具备灵活的单词拆分能力，支持驼峰命名和下划线命名等多种形式的单词拆分。无论您是在编写代码还是阅读文档，EnTranslate都是您的最佳翻译助手。";
         public const string Language = "zh-Hans";
-        public const string Version = "1.3.1";
-        public const string Author = "DDD D";
+        public const string Version = "1.3.2";
+        public const string Author = "Entity-now";
         public const string Tags = "";
     }
 }
